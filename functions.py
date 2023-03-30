5662356da43af18ebe705725b8c3c56f85522284
Muhammad Arbi Minanda <arbiminanda19@gmail.com>
2023-03-30 14:51:07 +0700

    split to functions & add input validation

---
+++ functions.py
@@ -0,0 +1,51 @@
+import sys
+
+class order():
+    def __init__(self, item, price, amount, total_price):
+        self.item = item
+        self.price = price
+        self.amount = amount
+        self.total_price = total_price
+
+def handleOrder(arr_items):
+    choose_items_done = False
+    orders = []
+    while not choose_items_done:
+        print("Choose items that you want add to cart: (input number of desired item)")
+        printItems(arr_items)
+        chosen_item = input("Input your option here: ")
+        if int(chosen_item) not in range(len(arr_items)):
+            print("Sorry, Error input")
+            sys.exit()
+        amount_item = input("How many do you want to buy that item? ")
+        orders.append(order(arr_items[int(chosen_item)-1].item, arr_items[int(chosen_item)-1].price, amount_item, (arr_items[int(chosen_item)-1].price)*int(amount_item)))
+        print("Any other items do you want to buy? (Answer 'y' for yes or 'n' for no)")
+        finish_order = input("Your Answer: ")
+        if (finish_order == "n"):
+            choose_items_done = True
+        elif (finish_order == "y"):
+            choose_items_done = False
+        else:
+            print("Sorry, Error input")
+            sys.exit()
+    return orders
+
+def printItems(arr_items):
+    i=0
+    while i<len(arr_items):
+        print(str(i+1) + ". Item Name: " + (arr_items[i].item).capitalize() + ", Price: Rp." + str(arr_items[i].price))
+        i += 1
+
+def printOrder(arr_items):
+    i=0
+    while i<len(arr_items):
+        print(str(i+1) + ". Item Name: " + (arr_items[i].item).capitalize() + ", Price: Rp." + str(arr_items[i].price) + ", Amount: " + str(arr_items[i].amount) + ", Total Price: Rp." + str(arr_items[i].total_price))
+        i += 1
+
+def calculateTotalOrderPrice(arr_orders):
+    i=0
+    total_price = 0  
+    while i<len(arr_orders):
+        total_price += arr_orders[i].total_price
+        i += 1
+    return total_price
--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from data import getAllItems, getPromoItems
 from messages import showActionsOptions, showWelcomingMessage
-from command import printItems, printOrder
+from functions import printItems, printOrder, handleOrder, calculateTotalOrderPrice
 
-class order():
-    def __init__(self, item, price, amount, total_price):
-        self.item = item
-        self.price = price
-        self.amount = amount
-        self.total_price = total_price
 all_items = getAllItems()
 promotional_items = getPromoItems()
 finish_shop = False
@@ -23,27 +17,14 @@ while not finish_shop:
         print("Here are list of all items that you can buy:")
         printItems(all_items)
     elif (action_input == "3"):
-        items_to_buy = []
-        choose_items_done = False
-        while not choose_items_done:
-            print("Choose items that you want add to cart: (input number of desired item)")
-            printItems(all_items)
-            chosen_item = input("Input your option here: ")
-            amount_item = input("How many do you want to buy that item? ")
-            items_to_buy.append(order(all_items[int(chosen_item)-1].item, all_items[int(chosen_item)-1].price, amount_item, (all_items[int(chosen_item)-1].price)*int(amount_item)))
-            print("Any other items do you want to buy? (Answer 'yes' or 'no')")
-            finish_order = input("Your Answer: ")
-            if (finish_order == "no"):
-                i=0
-                order_price = 0  
-                while i<len(items_to_buy):
-                    order_price += items_to_buy[i].total_price
-                    i += 1
-                print("Total Price of your order: Rp." + str(order_price))
-                print("Here are your order detail:")
-                printOrder(items_to_buy)
-                print("Thanks for buying our items ^_^")
-                choose_items_done = True
-    else:
+        items_to_buy = handleOrder(all_items)
+        print("Total Price of your order: Rp." + str(calculateTotalOrderPrice(items_to_buy)))
+        print("Here are your order detail:")
+        printOrder(items_to_buy)
+        print("Thanks for buying our items ^_^")
+    elif (action_input == "4"):
         print("Thanks for coming, have a nice day ^_^")
         finish_shop = True
+    else:
+        print("Sorry, Error input")
+        break
